5664c0ca59724d63054289486060cd8e30074eca
pedrohilan <pedro.hilan99@gmail.com>
2023-01-16 16:19:31 -0300

    add components

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css';
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+        <h3>Nóssss</h3>
+        <p>Mini blog &copy; 2023</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom"
+import styles from './Navbar.module.css'
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+        <NavLink to="/" className={styles.brand}>
+            Mini <span>blog</span>
+        </NavLink>
+        <ul className={styles.links_list}>
+            <li>
+                <NavLink to="/" className={({isActive}) => (isActive ? styles.active : '')}>Início</NavLink>
+            </li>
+            <li>
+                <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : '')}>Sobre</NavLink>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
